3d5a770e43824a96b919685f3ad341a7409d4548
Alex Grinman <grinman.alex@gmail.com>
2016-03-06 16:14:34 -0500

    new files, headers for scheme

--- DFA.go
+++ DFA.go
@@ -2,23 +2,22 @@ package alvis
 
 // Transition is the tuple of X -> Y on input C
 type Transition struct {
-    X int
-    Y int
-    C string
+	X int
+	Y int
+	C string
 }
+
 // DFA is a Deterministic Finite Automaton
 type DFA struct {
-    Representation string
-    States  []int
-    Alphabet []string
-    Transitions [][][]int
-    Start int
-    AcceptStates []int
+	Representation string
+	States         []int
+	Alphabet       []rune
+	Transitions    []Transition
+	Start          int
+	AcceptStates   []int
 }
 
 // GenDFA takes a regex string and outputs a DFA
 func GenDFA(regex string) DFA {
-    
+	return DFA{}
 }
-
-
