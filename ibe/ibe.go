cddee53a7ad33b6f958b5ae9168b527fe15f5e38
Alex Grinman <grinman.alex@gmail.com>
2016-05-31 20:50:24 -0400

    new working ibe scheme

--- ibe/ibe.go
+++ ibe/ibe.go
@@ -1,6 +1,11 @@
 package ibe
 
-import "github.com/Nik-U/pbc"
+import (
+	"crypto/sha256"
+	"fmt"
+
+	"github.com/Nik-U/pbc"
+)
 
 //////////////////////////////////////////////
 //MARK: Default Contants
@@ -22,6 +27,8 @@ type MasterKey struct {
 	T2 *pbc.Element
 	T3 *pbc.Element
 	T4 *pbc.Element
+
+	Params PublicParams
 }
 
 type PublicParams struct {
@@ -42,17 +49,32 @@ type PublicParams struct {
 }
 
 type PrivateKey struct {
+	D0 *pbc.Element
+	D1 *pbc.Element
+	D2 *pbc.Element
+	D3 *pbc.Element
+	D4 *pbc.Element
+}
+
+type CipherText struct {
+	C *pbc.Element
+
+	C0 *pbc.Element
+	C1 *pbc.Element
+	C2 *pbc.Element
+	C3 *pbc.Element
+	C4 *pbc.Element
 }
 
 //MARK: IBE Methods
 
 // DefaultSetup runs Setup with default inputs
-func DefaultSetup() (MasterKey, PublicParams, error) {
+func DefaultSetup() (MasterKey, PublicParams) {
 	return Setup(R, Q)
 }
 
 // Setup initializaes the MSK and PP
-func Setup(r uint32, q uint32) (msk MasterKey, pp PublicParams, err error) {
+func Setup(r uint32, q uint32) (msk MasterKey, pp PublicParams) {
 	pbc.SetCryptoRandom()
 
 	msk = MasterKey{}
@@ -81,12 +103,100 @@ func Setup(r uint32, q uint32) (msk MasterKey, pp PublicParams, err error) {
 	pp.V3 = pairing.NewG1().PowZn(pp.G, msk.T3)
 	pp.V4 = pairing.NewG1().PowZn(pp.G, msk.T4)
 
+	msk.Params = pp
+
 	return
 }
 
 // Encrypt encrpyts to an id a message m
-func (pp PublicParams) Encrypt(id string, m []byte) {
+func (msk MasterKey) Extract(id string) (sk PrivateKey) {
 	pbc.SetCryptoRandom()
 
-	return
+	pp := msk.Params
+	pairing := pp.Pairing
+
+	idEl := pairing.NewZr().SetBytes(SHA2(id))
+
+	r1, r2 := pairing.NewZr().Rand(), pairing.NewZr().Rand()
+
+	gog1_pid := pairing.NewG1().Mul(pp.G0, pairing.NewG1().PowZn(pp.G1, idEl))
+
+	//D0
+	d01 := pairing.NewZr().Mul(pairing.NewZr().Mul(r1, msk.T1), msk.T2)
+	d02 := pairing.NewZr().Mul(pairing.NewZr().Mul(r2, msk.T3), msk.T4)
+	sk.D0 = pairing.NewG1().PowZn(pp.G, pairing.NewZr().Add(d01, d02))
+
+	//D1
+	nw_t2 := pairing.NewZr().Mul(pairing.NewZr().Neg(msk.W), msk.T2)
+	nr1_t2 := pairing.NewZr().Mul(pairing.NewZr().Neg(r1), msk.T2)
+
+	sk.D1 = pairing.NewG1().Mul(pairing.NewG1().PowZn(pp.G, nw_t2), pairing.NewG1().PowZn(gog1_pid, nr1_t2))
+
+	//D2
+	nw_t1 := pairing.NewZr().Mul(pairing.NewZr().Neg(msk.W), msk.T1)
+	nr1_t1 := pairing.NewZr().Mul(pairing.NewZr().Neg(r1), msk.T1)
+	sk.D2 = pairing.NewG1().Mul(pairing.NewG1().PowZn(pp.G, nw_t1), pairing.NewG1().PowZn(gog1_pid, nr1_t1))
+
+	//D3
+	nr2_t4 := pairing.NewZr().Mul(pairing.NewZr().Neg(r2), msk.T4)
+	sk.D3 = pairing.NewG1().PowZn(gog1_pid, nr2_t4)
+
+	//D4
+	nr2_t3 := pairing.NewZr().Mul(pairing.NewZr().Neg(r2), msk.T3)
+	sk.D4 = pairing.NewG1().PowZn(gog1_pid, nr2_t3)
+
+	return sk
+}
+
+// Encrypt encrpyts to an id a message m
+func (pp PublicParams) Encrypt(id string, m []byte) (ctxt CipherText) {
+	pbc.SetCryptoRandom()
+
+	pairing := pp.Pairing
+
+	idEl := pairing.NewZr().SetBytes(SHA2(id))
+	mG := pairing.NewGT().SetBytes(m)
+
+	fmt.Println(len(mG.Bytes()))
+
+	s, s1, s2 := pairing.NewZr().Rand(), pairing.NewZr().Rand(), pairing.NewZr().Rand()
+
+	ctxt.C = pairing.NewGT().Mul(pairing.NewGT().PowZn(pp.O, s), mG)
+	ctxt.C0 = pairing.NewG1().PowZn(pairing.NewG1().Mul(pp.G0, pairing.NewG1().PowZn(pp.G1, idEl)), s)
+
+	ctxt.C1 = pairing.NewG1().PowZn(pp.V1, pairing.NewZr().Sub(s, s1))
+	ctxt.C2 = pairing.NewG1().PowZn(pp.V2, s1)
+	ctxt.C3 = pairing.NewG1().PowZn(pp.V3, pairing.NewZr().Sub(s, s2))
+	ctxt.C4 = pairing.NewG1().PowZn(pp.V4, s2)
+
+	return ctxt
+}
+
+// Encrypt encrpyts to an id a message m
+func (pp PublicParams) Decrypt(sk PrivateKey, ctxt CipherText) []byte {
+	pbc.SetCryptoRandom()
+
+	pairing := pp.Pairing
+
+	e0 := pairing.NewGT().Pair(ctxt.C0, sk.D0)
+	e1 := pairing.NewGT().Pair(ctxt.C1, sk.D1)
+	e2 := pairing.NewGT().Pair(ctxt.C2, sk.D2)
+	e3 := pairing.NewGT().Pair(ctxt.C3, sk.D3)
+	e4 := pairing.NewGT().Pair(ctxt.C4, sk.D4)
+
+	mG := pairing.NewGT().Mul(ctxt.C, pairing.NewGT().Mul(e0, pairing.NewGT().Mul(e1, pairing.NewGT().Mul(e2, pairing.NewGT().Mul(e3, e4)))))
+
+	return mG.Bytes()
+}
+
+//MARK: Helpers
+
+func (pp PublicParams) ValBytes(m []byte) []byte {
+	return pp.Pairing.NewGT().SetBytes(m).Bytes()
+}
+
+func SHA2(data string) []byte {
+	h := sha256.New()
+	h.Write([]byte(data))
+	return h.Sum(nil)
 }
---
+++ ibe/ibe_test.go
@@ -0,0 +1,73 @@
+package ibe
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
+
+	os.Exit(m.Run())
+}
+
+// TestGenAndEval tests functionality
+func TestIBE(t *testing.T) {
+	msk, pp := DefaultSetup()
+	sk := msk.Extract("feugiat")
+
+	m := []byte{0xAA}
+	c := pp.Encrypt("feugiat", m)
+
+	ret := pp.Decrypt(sk, c)
+	fmt.Println()
+
+	exp := pp.ValBytes(m)
+	if len(exp) != len(ret) {
+		t.Errorf("Got back: %d. \nExpected %d", ret, exp)
+		return
+	}
+
+	success := true
+	last := 0
+	for i := range ret {
+		last = i
+
+		if ret[i] != exp[i] {
+			success = false
+			break
+		}
+	}
+
+	if !success {
+		t.Errorf("Error at byte %d.\nGot back: %d. \nExpected %d", last, ret, exp)
+		return
+	}
+}
+
+// func TestIBEParallel(t *testing.T) {
+// 	_, pp := DefaultSetup()
+//
+// 	var wg sync.WaitGroup
+// 	wg.Add(1000)
+//
+// 	for i := 0; i < 1000; i++ {
+// 		go func(w *sync.WaitGroup) {
+// 			_ = pp.Encrypt("feugiatfeugiatfeugiatfeugiat", []byte{0x01})
+// 			w.Done()
+// 		}(&wg)
+// 	}
+//
+// 	wg.Wait()
+// }
+
+func BenchmarkIBEEncrypt(b *testing.B) {
+	_, pp := DefaultSetup()
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		_ = pp.Encrypt("feugiatfeugiatfeugiatfeugiat", []byte{0x01})
+	}
+}
